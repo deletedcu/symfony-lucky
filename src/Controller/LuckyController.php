da36c25ed5c58936657183d101cb4b30cbb76788
CUREST TREBOW <43941751+curest0x1021@users.noreply.github.com>
2018-11-26 23:12:56 +0800

    Add annotations

---
+++ src/Controller/LuckyController.php
@@ -0,0 +1,19 @@
+<?php
+// src/Controller/LuckyController.php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LuckyController {
+    /**
+     * @Route("/lucky/number")
+     */
+    public function number() {
+        $number = random_int(0, 100);
+
+        return new Response(
+            '<html><body>Lucky number: '.$number.'</body></html>'
+        );
+    }
+}
