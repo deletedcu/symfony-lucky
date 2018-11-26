89d5ac522487132a20c7aeaf9651507cf9019bcf
CUREST TREBOW <43941751+curest0x1021@users.noreply.github.com>
2018-11-26 23:24:49 +0800

    Integrate the twig

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -4,16 +4,17 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class LuckyController {
+class LuckyController extends AbstractController {
     /**
      * @Route("/lucky/number")
      */
     public function number() {
         $number = random_int(0, 100);
 
-        return new Response(
-            '<html><body>Lucky number: '.$number.'</body></html>'
-        );
+        return $this->render('lucky/number.html.twig', [
+            'number' => $number,
+        ]);
     }
 }
